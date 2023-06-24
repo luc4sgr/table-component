70f3bdd8f9a21bf33396ed14053ec956debbdfa2
Lucas Granjense <74532410+luc4sgr@users.noreply.github.com>
2023-06-24 10:13:05 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+export function App() {
+  
+  const styles = {
+    title:"text-3xl font-bold underline text-cyan-500"
+  }
+
+  return (
+    <>
+      <h1 className={styles.title}> 
+        Hello dev, good job.
+      </h1>
+    </>
+  );
+}
+
